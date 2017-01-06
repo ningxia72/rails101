b32a0ab866016554268269db95d95f2155f5759a
ningxia72 <ningxia72@gmail.com>
2017-01-06 16:19:17 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ApplicationRecord
            :recoverable, :rememberable, :trackable, :validatable
 
     has_many :groups
+    has_many :posts
     has_many :group_relationships
     has_many :participated_groups, :through => :group_relationships, :source => :group
-    has_many :posts
+
     def is_member_of?(group)
      participated_groups.include?(group)
     end
+    def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
   end
